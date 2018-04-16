1eaf24f2d70239dc485731e49f630cbefd3ed2e8
thietnnt2210 <thietnnt2210@gmail.com>
2018-04-16 09:46:46 +0700

    bill controller

--- controllers/BillController.php
+++ controllers/BillController.php
@@ -45,40 +45,40 @@ class BillController extends Controller
         ]);
     }
 
-//    public function actionCreate()
-//    {
-//
-//        $comboboxdata = Yii::$app->db->createCommand('SELECT * FROM unit')
-//            ->queryAll();
-//
-//
-//        if (Yii::$app->request->post()) {
-//            $idchungtu = $_REQUEST['idchungtu'];
-//            $idproduct = $_REQUEST['idproduct'];
-//            $productname = $_REQUEST['productname'];
-//            $unit = $_REQUEST['unit'];
-//            $count = $_REQUEST['count'];
-//            $price = $_REQUEST['price'];
-//            $cost = $_REQUEST['cost'];
-//            $idnhacungcap = $_REQUEST['idnhacungcap'];
-//            $note = $_REQUEST['note'];
-//            $date = $_REQUEST['date'];
-//
-//            $sql = "insert into warehouse (idchungtu,idproduct,productname,unit,count,price,cost,idnhacungcap,note,date)
-//                    values (:idchungtu,:idproduct,:productname,:unit,:idnhacungcap,:note,:date)";
-//
-//            $parameters = array("idchungtu" => $idchungtu, "idproduct" => $idproduct, "productname" => $productname,
-//                "unit" => $unit, "count" => $count, "price" => $price, "cost" => $cost, "idnhacungcap" => $idnhacungcap, "note" => $note, "date" => $date);
-//            // Yii::$app->db->createCommand($sql)->insert($parameters)->execute();
-//
-//            Yii::$app->db->createCommand()->insert('warehouse', $parameters)->execute();
-//            return $this->redirect(['/bill/index']);
-//        }
-//
-//        return $this->render('create', [
-//            'comboboxdata' => $comboboxdata
-//        ]);
-//    }
+    public function actionCreate()
+    {
+
+        $comboboxdata = Yii::$app->db->createCommand('SELECT * FROM unit')
+            ->queryAll();
+
+
+        if (Yii::$app->request->post()) {
+            $idchungtu = $_REQUEST['idchungtu'];
+            $idproduct = $_REQUEST['idproduct'];
+            $productname = $_REQUEST['productname'];
+            $unit = $_REQUEST['unit'];
+            $count = $_REQUEST['count'];
+            $price = $_REQUEST['price'];
+            $cost = $_REQUEST['cost'];
+            $idnhacungcap = $_REQUEST['idnhacungcap'];
+            $note = $_REQUEST['note'];
+            $date = $_REQUEST['date'];
+
+            $sql = "insert into warehouse (idchungtu,idproduct,productname,unit,count,price,cost,idnhacungcap,note,date)
+                    values (:idchungtu,:idproduct,:productname,:unit,:idnhacungcap,:note,:date)";
+
+            $parameters = array("idchungtu" => $idchungtu, "idproduct" => $idproduct, "productname" => $productname,
+                "unit" => $unit, "count" => $count, "price" => $price, "cost" => $cost, "idnhacungcap" => $idnhacungcap, "note" => $note, "date" => $date);
+            // Yii::$app->db->createCommand($sql)->insert($parameters)->execute();
+
+            Yii::$app->db->createCommand()->insert('warehouse', $parameters)->execute();
+            return $this->redirect(['/bill/index']);
+        }
+
+        return $this->render('create', [
+            'comboboxdata' => $comboboxdata
+        ]);
+    }
 
     public function actionUpdate()
     {
