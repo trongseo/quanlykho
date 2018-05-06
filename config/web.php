63a611f0377eb3d40ff187bbfaa3ea32c06ddb8e
nvtrong <nvtrong@starboardasia.com>
2018-05-06 14:31:02 +0700

    add

--- config/web.php
+++ config/web.php
@@ -3,6 +3,7 @@
 $params = require(__DIR__ . '/params.php');
 
 $config = [
+	'defaultRoute' => 'delivery/index',
     'id' => 'basic',
     'basePath' => dirname(__DIR__),
     'bootstrap' => ['log'],
